4fc38ece0484e555ce81456afcd4cef57039c7c7
Bartłomiej Strama <kontakt@bstrama.com>
2024-08-26 15:15:41 +0200

    counter added

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::sync::{Arc, Mutex};
 use rayon::prelude::*;
 
 fn main() {
@@ -22,14 +23,16 @@ fn main() {
     let board_size = board_size.trim().parse::<u8>().expect("You should pass int value");
 
     let starting_point = (x, y);
+    
+    let mut how_many_iterations = Arc::new(Mutex::new(0));
 
-    let knight_path: Vec<(u8, u8)> = find_chess_knight_path(board_size, vec![starting_point.clone()]).unwrap();
+    let knight_path: Vec<(u8, u8)> = find_chess_knight_path(board_size, vec![starting_point.clone()], &mut how_many_iterations).unwrap();
 
     println!("\n\nWe finally got a results:");
     println!("{:?}", knight_path);
 }
 
-fn find_chess_knight_path(board_size: u8, moves_already_done: Vec<(u8, u8)>) -> Option<Vec<(u8, u8)>> {
+fn find_chess_knight_path(board_size: u8, moves_already_done: Vec<(u8, u8)>, how_many_iterations: &mut Arc<Mutex<u128>>) -> Option<Vec<(u8, u8)>> {
     let knight_position = *moves_already_done.last()
         .expect("Error, vector with default position should have at least 1 element");
     let (x, y) = (knight_position.0 as i8, knight_position.1 as i8);
@@ -43,6 +46,14 @@ fn find_chess_knight_path(board_size: u8, moves_already_done: Vec<(u8, u8)>) -> 
         (x - 1, y + 2),
         (x - 1, y - 2)
     ];
+    
+    let how_many_iterations_clone = how_many_iterations.clone();
+    let mut iterations = how_many_iterations_clone.lock().unwrap();
+    *iterations += 1;
+    if *iterations % 1000000 == 0 {
+        println!("Iterations: 1 milion * {}", *iterations / 1000000);
+    }
+    drop(iterations);
 
     let possible_moves: Vec<(u8, u8)> = possible_moves
         .into_iter()
@@ -56,7 +67,8 @@ fn find_chess_knight_path(board_size: u8, moves_already_done: Vec<(u8, u8)>) -> 
         .map(|&potential_knight_position| {
             let mut moves_that_will_be_done = moves_already_done.clone();
             moves_that_will_be_done.push(potential_knight_position);
-            find_chess_knight_path(board_size, moves_that_will_be_done.clone())
+            let mut how_many_iterations = how_many_iterations.clone();
+            find_chess_knight_path(board_size, moves_that_will_be_done.clone(), &mut how_many_iterations)
                 .unwrap_or_else(|| moves_that_will_be_done)
         })
         .max_by_key(|vec| vec.len());
